47e5f2910b8a8115cb924fa67b6d339d5991fd67
kieran <46075444+The-Angry-007@users.noreply.github.com>
2024-03-25 16:17:10 +0000

    added line tally

---
+++ lineTally.py
@@ -0,0 +1,12 @@
+#quick script to find the number of lines of code in the src folder
+import os
+directory = os.getcwd() + "\\src"
+total_lines = 0
+for root, dirs, files in os.walk(directory):
+	if "Platform" in root or "Utility" in root:
+		continue
+	for file in files:
+		file_path = os.path.join(root,file)
+		with open(file_path,'r', encoding = "utf-8", errors="ignore") as f:
+			total_lines += len(f.readlines())
+print("Directory contains",total_lines, "lines of code")
